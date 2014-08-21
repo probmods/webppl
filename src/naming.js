ac010c7df51169dd33d3b511107ae78ad995c45f
Noah Goodman <ngoodman@stanford.edu>
2014-08-21 04:47:37 -0700

    added simple naming transform

---
+++ src/naming.js
@@ -0,0 +1,100 @@
+"use strict";
+
+var assert = require('assert');
+var _ = require('underscore');
+var estraverse = require("estraverse");
+var escodegen = require("escodegen");
+var esprima = require("esprima");
+var estemplate = require("estemplate");
+var types = require("ast-types");
+var util = require('./util.js');
+
+var build = types.builders;
+var Syntax = estraverse.Syntax;
+
+var gensym = util.makeGensym();
+function makeGensym(name){
+  return build.identifier("_".concat(gensym(name)));
+}
+
+var counter = 0
+function nextCounter(){
+  counter++
+  return build.arrayExpression([build.literal(counter)])
+}
+
+var addressIdNode = build.identifier("address")
+
+function makeAddressExtension(){
+  return build.callExpression(build.memberExpression(addressIdNode,
+                                                     build.identifier("concat"),
+                                                     false),
+                              [nextCounter()])
+}
+
+function naming(node) {
+//  console.log(node)
+  switch (node.type) {
+      
+     
+      //have to add an address argument to each function
+    case Syntax.FunctionExpression:
+      return build.functionExpression(node.id,
+                                      [addressIdNode].concat(node.params),
+                                      node.body)
+      
+      //add a gensym onto the address variable
+    case Syntax.CallExpression:
+      return build.callExpression(node.callee,
+                                  [makeAddressExtension()].concat(node.arguments))
+      
+      
+      
+//      //binary and unary are actually calls, but deterministic so don't need to extend address..
+//    case Syntax.UnaryExpression:
+//    case Syntax.BinaryExpression:
+      
+//    case Syntax.IfStatement:
+//    case Syntax.ConditionalExpression:
+//    case Syntax.ArrayExpression:
+//    case Syntax.ObjectExpression:
+//    case Syntax.MemberExpression:
+      
+//    case Syntax.BlockStatement:
+//      return cpsBlock(node.body, cont);
+//      
+//    case Syntax.Program:
+//      return
+//      
+//    case Syntax.ReturnStatement:
+//      return
+//      
+//    case Syntax.ExpressionStatement:
+//      return
+//      
+//    case Syntax.EmptyStatement:
+//    case Syntax.Identifier:
+//    case Syntax.Literal:
+//    case Syntax.VariableDeclaration:
+      
+      
+    default:
+      return node
+
+  }
+}
+
+
+function namingMain(node) {
+  
+  return estraverse.replace(node,
+                             {//enter: function(node){return node},
+                             leave: function(node){return naming(node)}})
+}
+
+
+
+
+module.exports = {
+naming: namingMain
+};
