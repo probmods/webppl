9f124ec55277e4f10af3fdae44cece87fa1c1bd6
Pedram Razavi <pedram.razavi@gmail.com>
2015-01-30 18:47:05 -0800

    Refactor and move hard-coded wppl test cases to individual files.

--- tests/test-inference.js
+++ tests/test-inference.js
@@ -1,7 +1,9 @@
 "use strict";
 
+var fs = require('fs');
 var util = require("../src/util.js");
 var webppl = require('../src/main.js');
+var testCase = require('nodeunit').testCase;
 
 var topK;
 var _trampoline;
@@ -11,7 +13,7 @@ function runContinuousSamplingTest(test, code, checkSamples, numSamples){
   topK = function(s, value){
     _trampoline = null;
     samples.push(value);
-    if (samples.length == numSamples){
+    if (samples.length === numSamples){
       test.ok(checkSamples(samples));
       test.done();
     }
@@ -20,7 +22,7 @@ function runContinuousSamplingTest(test, code, checkSamples, numSamples){
   for (var i=0; i<numSamples; i++){
     eval('(function(){' + compiledProgram + '})()');
   }
-};
+}
 
 function runDiscreteSamplingTest(test, code, expectedHist, numSamples, tolerance){
   var hist = {};
@@ -30,7 +32,7 @@ function runDiscreteSamplingTest(test, code, expectedHist, numSamples, tolerance
     hist[value] = hist[value] || 0;
     hist[value] += 1;
     numFinishedSamples += 1;
-    if (numFinishedSamples == numSamples){
+    if (numFinishedSamples === numSamples){
       var normHist = util.normalizeHist(hist);
       test.ok(util.histsApproximatelyEqual(normHist, expectedHist, tolerance));
       test.done();
@@ -40,7 +42,7 @@ function runDiscreteSamplingTest(test, code, expectedHist, numSamples, tolerance
   for (var i=0; i<numSamples; i++){
     eval('(function(){' + compiledProgram + '})()');
   }
-};
+}
 
 function runDistributionTest(test, code, expectedHist, tolerance){
   var hist = {};
@@ -55,7 +57,7 @@ function runDistributionTest(test, code, expectedHist, tolerance){
     test.done();
   };
   webppl.run(code, topK);
-};
+}
 
 exports.testDeterministic = {
 
@@ -65,7 +67,6 @@ exports.testDeterministic = {
     var tolerance = 0.0001; // in case of floating point errors
     return runDiscreteSamplingTest(test, code, expectedHist, 1, tolerance);
   }
-
 };
 
 exports.testForwardSampling = {
@@ -73,10 +74,10 @@ exports.testForwardSampling = {
   testApplication: function (test) {
     var code = "flip(.5) & flip(.5)";
     var expectedHist = {
-      1: .25,
-      0: .75
+      1: 0.25,
+      0: 0.75
     };
-    var tolerance = .05;
+    var tolerance = 0.05;
     var numSamples = 1000;
     return runDiscreteSamplingTest(test, code, expectedHist, numSamples, tolerance);
   },
@@ -90,7 +91,7 @@ exports.testForwardSampling = {
       3: 0.0064,
       4: 0.00128
     };
-    var tolerance = .05;
+    var tolerance = 0.05;
     var numSamples = 1000;
     return runDiscreteSamplingTest(test, code, expectedHist, numSamples, tolerance);
   },
@@ -98,13 +99,13 @@ exports.testForwardSampling = {
   testRandomInteger: function(test) {
     var code = "randomInteger(5)";
     var expectedHist= {
-      0: .2,
-      1: .2,
-      2: .2,
-      3: .2,
-      4: .2
+      0: 0.2,
+      1: 0.2,
+      2: 0.2,
+      3: 0.2,
+      4: 0.2
     };
-    var tolerance = .05;
+    var tolerance = 0.05;
     var numSamples = 1000;
     return runDiscreteSamplingTest(test, code, expectedHist, numSamples, tolerance);
   },
@@ -132,123 +133,52 @@ exports.testForwardSampling = {
         samples.map(function(x){return Math.pow(x - empiricalMean, 2);})) / samples.length;
       var expectedVariance = 1/12 * Math.pow(5-3, 2);
       var expectedMean = 4;
-      return ((Math.abs(empiricalVariance - expectedVariance) < .2) &&
-              (Math.abs(empiricalMean - expectedMean) < .2));
+      return ((Math.abs(empiricalVariance - expectedVariance) < 0.2) &&
+              (Math.abs(empiricalMean - expectedMean) < 0.2));
     };
     return runContinuousSamplingTest(test, code, check, numSamples);
   }
 };
 
-exports.testEnumeration = {
-  test1: function(test){
-    var code = ("Enumerate(" +
-                "  function(){" +
-                "    var x = flip(0.5);" +
-                "    var y = flip(0.5);" +
-                "    factor( (x|y) ? 0 : -Infinity);" +
-                "    return x;" +
-                "  }," +
-                "  300) // particles");
-    var expectedHist = {
-      "true": 2/3,
-      "false": 1/3
-    };
-    var tolerance = .1;
-    runDistributionTest(test, code, expectedHist, tolerance);
-  },
+function getTestCases(testNames) {
+	var rootDirectory = "./tests/test-data/";
+	var testCases = [];
+	for (var i = 0; i < testNames.length; i++) {
+		var codeFileName = rootDirectory + testNames[i] + ".wppl";
+		var resultFileName = rootDirectory + testNames[i] + ".json";
+		var codeFile = fs.readFileSync(codeFileName, "utf-8");
+		var expectedResult = JSON.parse(fs.readFileSync(resultFileName, "utf-8"));
+		testCases.push({
+			code: codeFile,
+			expectedHist: expectedResult.expectedHist,
+			tolerance: expectedResult.tolerance,
+			name: testNames[i]
+		});
+	}
+	return testCases;
+}
 
-  test2: function(test){
-    var code = ("var e = cache(function (x){" +
-                "    return Enumerate(function() {" +
-                "                     var a = flip(0.5) & flip(0.5);" +
-                "                     factor(a? 2 : Math.log(0.3));" +
-                "                     return a & x;" +
-                "                     });});" +
-                "" +
-                "Enumerate(function(){" +
-                "            var e1 = sample(e(true));" +
-                "            var e2 = sample(e(true));" +
-                "            return e1 & e2;" +
-                "          });");
-    // TODO: Check that the expected hist is correct
-    var expectedHist = { '0': 0.2053648535282959, '1': 0.794635146471704 };
-    var tolerance = .0001;
-    runDistributionTest(test, code, expectedHist, tolerance);
-  }
-};
+function makeTest(testData) {
+	function _makeTest (test) {
+		runDistributionTest(test, testData.code, testData.expectedHist, testData.tolerance);
+	}
+	return _makeTest;
+}
 
-exports.testParticleFilter = {
-  test1: function(test){
-    var code = ("ParticleFilter(" +
-                "  function(){" +
-                "    var x = flip(0.5);" +
-                "    var y = flip(0.5);" +
-                "    factor( (x|y) ? 0 : -Infinity);" +
-                "    return x;" +
-                "  }," +
-                "  1000) // particles");
-    var expectedHist = {
-      "true": 2/3,
-      "false": 1/3
-    };
-    var tolerance = .1;
-    runDistributionTest(test, code, expectedHist, tolerance);
-  }
-};
+var testNames = [
+	"testEnumeration",
+	"testEnumerationCached",
+	"testParticleFilter",
+	"testMH",
+	"testPMCMC",
+	"testPFRj"];
 
-exports.testMH = {
-  test1: function(test){
-    var code = ("MH(" +
-                "  function(){" +
-                "    var x = flip(0.5);" +
-                "    var y = flip(0.5);" +
-                "    factor( (x|y) ? 0 : -Infinity);" +
-                "    return x;" +
-                "  }," +
-                "  5000) // samples");
-    var expectedHist = {
-      "true": 2/3,
-      "false": 1/3
-    };
-    var tolerance = .1;
-    runDistributionTest(test, code, expectedHist, tolerance);
-  }
-};
+var testsData = getTestCases(testNames);
 
-exports.testPMCMC = {
-  test1: function(test){
-    var code = ("PMCMC(" +
-                "  function(){" +
-                "    var x = flip(0.5);" +
-                "    var y = flip(0.5);" +
-                "    factor( (x|y) ? 0 : -Infinity);" +
-                "    return x;" +
-                "  }," +
-                "  1000, 5) // particles");
-    var expectedHist = {
-      "true": 2/3,
-      "false": 1/3
-    };
-    var tolerance = .1;
-    runDistributionTest(test, code, expectedHist, tolerance);
-  }
-};
-
-exports.testPFRj = {
-  test1: function(test){
-    var code = ("ParticleFilterRejuv(" +
-                "  function(){" +
-                "    var x = flip(0.5);" +
-                "    var y = flip(0.5);" +
-                "    factor( (x|y) ? 0 : -Infinity);" +
-                "    return x;" +
-                "  }," +
-                "  1000, 10) // particles, rejuvenation steps");
-    var expectedHist = {
-      "true": 2/3,
-      "false": 1/3
-    };
-    var tolerance = .1;
-    runDistributionTest(test, code, expectedHist, tolerance);
-  }
-};
+for (var i=0; i < testsData.length; i++) {
+	var testData = testsData[i];
+	var description = testData.desc ? testData.desc : "test";
+	var testCaseArgs = {};
+	testCaseArgs[description] = makeTest(testData);
+	exports[testData.name] = testCase(testCaseArgs);
+}
