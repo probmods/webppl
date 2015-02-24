2e83944a141d94613017bbcc709afc256eefe17b
Pedram Razavi <pedram.razavi@gmail.com>
2015-02-23 16:46:37 -0800

    Add nodeunit tests and jshint to Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,30 @@
+'use strict';
+
 module.exports = function(grunt) {
   grunt.initConfig({
-    gjslint: {
-        options: {
-          flags: [
-            '--flagfile grunt_tasks/.gjslintrc'
-          ],
-          reporter: {
-            name: 'console'
-          },
-          force: false
-        },
-        lib: {
-          src: ['src/*.js', 'Gruntfile.js']
-        },
-        test: {
-          src: ['tests/*.js']
-        },
-        wppl: {
-          src: ['examples/*.wppl', 'tests/test-data/*.wppl'],
-          additionalFlags: [
-            '--disable 10,11',
-            '--additional_extensions=wppl'
-          ]
-        }
+    nodeunit: {
+      all: ['tests/*.js']
     },
-    fixjstyle: {
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js', 'tests/*.js'],
+      options: {
+        nonew: true,
+        curly: true,
+        noarg: true,
+        trailing: true,
+        forin: true,
+        noempty: true,
+        node: true,
+        eqeqeq: true,
+        strict: true,
+        evil: true,
+        undef: true,
+        bitwise: true,
+        browser: true,
+        gcl: true
+      }
+    },
+    gjslint: {
       options: {
         flags: [
           '--flagfile grunt_tasks/.gjslintrc'
@@ -43,15 +43,37 @@ module.exports = function(grunt) {
       wppl: {
         src: ['examples/*.wppl', 'tests/test-data/*.wppl'],
         additionalFlags: [
-          '--disable 10,11',
+          '--disable 10,11', // Disable semicolon errors.
           '--additional_extensions=wppl'
         ]
+      }
+    },
+    fixjsstyle: {
+      options: {
+        flags: [
+          '--flagfile grunt_tasks/.gjslintrc'
+        ],
+        reporter: {
+          name: 'console'
+        },
+        force: false
+      },
+      lib: {
+        src: ['src/*.js', 'Gruntfile.js']
+      },
+      test: {
+        src: ['tests/*.js']
       }
     }
   });
 
   grunt.loadTasks('grunt_tasks');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Default task(s).
-  grunt.registerTask('default', ['gjslint']);
+  grunt.registerTask('default', ['nodeunit', 'gjslint']);
+  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('lint', ['gjslint']);
+  grunt.registerTask('jshint', ['jshint']);
+  grunt.registerTask('fixstyle', ['fixjsstyle']);
 };
