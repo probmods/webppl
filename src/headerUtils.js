e7d906ba86eaea07e74fa083f457f59a4c15b006
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2015-03-29 19:07:03 -0700

    Version of cache that works on object identity, not value, for implementing immutable data structures.

--- src/headerUtils.js
+++ src/headerUtils.js
@@ -34,10 +34,35 @@ module.exports = function(env) {
     return wpplFn.apply(global, [s, k, a].concat(args));
   }
 
+  // unique-id-based memoization
+  var id = 0;
+  function mem(s, k, a, f) {
+    var c = {};
+    var cf = function(s, k, a) {
+      var args = Array.prototype.slice.call(arguments, 3);
+      var argids = args.map(function(a) {
+        return a.__memid === undefined ? a : a.__memid;
+      });
+      var key = JSON.stringify(argids);
+      if (key in c) {
+        return k(s, c[key]);
+      } else {
+        var newk = function(s, r) {
+          r.__memid = id; id++;
+          c[key] = r;
+          return k(s, r);
+        };
+        return f.apply(this, [s, newk, a].concat(args));
+      }
+    };
+    return k(s, cf);
+  }
+
   return {
     display: display,
     cache: cache,
-    apply: apply
+    apply: apply,
+    mem: mem
   };
 
 };
