7d2d9be6d21ad0d90621c6aa48c298c19f3d364a
Pedram Razavi <prazavi@mit.edu>
2015-04-15 11:25:35 -0700

    Update graph.py to get multidimensional traces.

--- src/inference/mh-diagnostics/graph.py
+++ src/inference/mh-diagnostics/graph.py
@@ -3,13 +3,27 @@ import pymc
 import numpy as np
 import matplotlib.pyplot as plt
 
-temp_path = './src/inference/mh-diagnostics/temp'
+def get_traces(temp_path):
+    with open(temp_path + '/trace.json', 'r') as data_file:
+        vals = np.array(json.load(data_file))
 
-with open(temp_path + '/trace.json', 'r') as data_file:
-    trace = np.array(json.load(data_file))
+    if type(vals[0]) != list:
+        vals = [[v] for v in vals]
 
-pymc.Matplot.plot(trace, name='trace', path=temp_path, verbose=0)
-scores = pymc.geweke(trace)
-pymc.Matplot.geweke_plot(scores, name='gweke', path=temp_path)
+    traces = np.zeros((len(vals[0]), len(vals)))
+    for i in range(len(vals)):
+        for j in range(len(vals[i])):
+            traces[j][i] = vals[i][j]
+    return traces
 
-plt.show()
+def plot(temp_path, traces):
+    for i in range(len(traces)):
+        pymc.Matplot.plot(traces[i], name='trace_' + str(i), path=temp_path, verbose=0)
+        scores = pymc.geweke(traces[i])
+        pymc.Matplot.geweke_plot(scores, name='gweke_' + str(i), path=temp_path)
+
+if __name__ == '__main__':
+    temp_path = './src/inference/mh-diagnostics/temp'
+    traces = get_traces(temp_path)
+    plot(temp_path, traces)
+    plt.show()
