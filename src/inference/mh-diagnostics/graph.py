e6d76387650362d1c9863cc04404edec6b2d08d4
Pedram Razavi <prazavi@mit.edu>
2015-04-16 22:19:02 -0700

    Update graph.py so that gweke doesn't fail on a sparse trace.

--- src/inference/mh-diagnostics/graph.py
+++ src/inference/mh-diagnostics/graph.py
@@ -19,8 +19,11 @@ def get_traces(temp_path):
 def plot(temp_path, traces):
     for i in range(len(traces)):
         pymc.Matplot.plot(traces[i], name='trace_' + str(i), path=temp_path, verbose=0)
-        scores = pymc.geweke(traces[i])
-        pymc.Matplot.geweke_plot(scores, name='gweke_' + str(i), path=temp_path)
+        try:
+            scores = pymc.geweke(traces[i])
+            pymc.Matplot.geweke_plot(scores, name='gweke_' + str(i), path=temp_path)
+        except:
+            print 'Failed to create Geweke plot.'
 
 if __name__ == '__main__':
     temp_path = './src/inference/mh-diagnostics/temp'
