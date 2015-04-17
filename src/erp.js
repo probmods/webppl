7d2c3f22ed6738659747c3971b51a7fd00ca201d
Pedram Razavi <prazavi@mit.edu>
2015-04-16 22:16:33 -0700

    Remove MH proposalParams from ERPs.

--- src/erp.js
+++ src/erp.js
@@ -28,9 +28,9 @@ function ERP(sampler, scorer, auxParams) {
   auxParams = typeof auxParams === 'undefined' ? {} : auxParams;
   this.sample = sampler;
   this.score = scorer;
-  for (var param in auxParams) {
-    if (auxParams.hasOwnProperty(param)) {
-      this[param] = auxParams[param];
+  for (var key in auxParams) {
+    if (auxParams.hasOwnProperty(key)) {
+      this[key] = auxParams[key];
     }
   }
 }
@@ -46,7 +46,7 @@ var uniformERP = new ERP(
       }
       return -Math.log(params[1] - params[0]);
     }
-    );
+);
 
 var bernoulliERP = new ERP(
     function flipSample(params) {
@@ -71,7 +71,7 @@ var bernoulliERP = new ERP(
         return val ? [1 / weight] : [-1 / weight];
       }
     }
-    );
+);
 
 
 var randomIntegerERP = new ERP(
@@ -88,7 +88,7 @@ var randomIntegerERP = new ERP(
         return _.range(params[0]);
       }
     }
-    );
+);
 
 function gaussianSample(params) {
   var mu = params[0];
@@ -110,13 +110,7 @@ function gaussianScore(params, x) {
   return -0.5 * (LOG_2PI + 2 * Math.log(sigma) + (x - mu) * (x - mu) / (sigma * sigma));
 }
 
-function gaussianProposalParams(params, prevVal, scaling) {
-  var mu = prevVal || params[0];
-  var sigma = params[1] * scaling;
-  return [mu, sigma];
-}
-
-var gaussianERP = new ERP(gaussianSample, gaussianScore, {proposalParams: gaussianProposalParams});
+var gaussianERP = new ERP(gaussianSample, gaussianScore);
 
 function multivariateGaussianSample(params) {
   var mu = params[0];
@@ -156,7 +150,7 @@ var discreteERP = new ERP(
             return _.range(params[0].length);
           }
     }
-    );
+);
 
 var gammaCof = [
   76.18009172947146,
@@ -209,7 +203,7 @@ var gammaERP = new ERP(
       var x = val;
       return (a - 1) * Math.log(x) - x / b - logGamma(a) - a * Math.log(b);
     }
-    );
+);
 
 var exponentialERP = new ERP(
     function exponentialSample(params) {
@@ -221,7 +215,7 @@ var exponentialERP = new ERP(
       var a = params[0];
       return Math.log(a) - a * val;
     }
-    );
+);
 
 function logBeta(a, b) {
   return logGamma(a) + logGamma(b) - logGamma(a + b);
@@ -244,7 +238,7 @@ var betaERP = new ERP(
           (a - 1) * Math.log(x) + (b - 1) * Math.log(1 - x) - logBeta(a, b) :
           -Infinity);
     }
-    );
+);
 
 function binomialG(x) {
   if (x === 0) {
@@ -325,7 +319,7 @@ var binomialERP = new ERP(
             return _.range(params[1]).concat([params[1]]);
           }
     }
-    );
+);
 
 function fact(x) {
   var t = 1;
@@ -381,7 +375,7 @@ var poissonERP = new ERP(
       var k = val;
       return k * Math.log(mu) - mu - lnfact(k);
     }
-    );
+);
 
 function dirichletSample(params) {
   var alpha = params;
@@ -414,13 +408,7 @@ function dirichletScore(params, val) {
   return logp;
 }
 
-function dirichletProposalParams(params, prevVal, x) {
-  var concentration = 0.01; // arbitrary
-  var driftParams = params.map(function(x) {return concentration * x});
-  return driftParams;
-}
-
-var dirichletERP = new ERP(dirichletSample, dirichletScore, {proposalParams: dirichletProposalParams});
+var dirichletERP = new ERP(dirichletSample, dirichletScore);
 
 function multinomialSample(theta) {
   var thetaSum = util.sum(theta);
@@ -521,5 +509,15 @@ module.exports = {
   randomIntegerERP: randomIntegerERP,
   uniformERP: uniformERP,
   makeMarginalERP: makeMarginalERP,
+<<<<<<< HEAD
   makeDeltaERP: makeDeltaERP
+=======
+  gaussianSample: gaussianSample,
+  gaussianScore: gaussianScore,
+  dirichletSample: dirichletSample,
+  dirichletScore: dirichletScore
+>>>>>>> Remove MH proposalParams from ERPs.
 };
+
+
+// mh
