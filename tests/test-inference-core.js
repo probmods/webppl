5fdffa4e2377cca9f9a6257807ceb0a93795a64d
null-a <null-a@users.noreply.github.com>
2015-06-11 13:14:50 +0100

    Clean-up new inference tests a little.

--- tests/test-inference-core.js
+++ tests/test-inference-core.js
@@ -8,7 +8,7 @@ var webppl = require('../src/main.js');
 
 var testDataDir = './tests/test-data/';
 
-var inferenceProcs = [
+var testDefinitions = [
   {
     name: 'Enumerate',
     args: [10],
@@ -44,7 +44,7 @@ var inferenceProcs = [
     func: 'ParticleFilterRejuv',
     args: [1, 10000],
     only: ['simple'],
-    tol: { hist: 0.1 },
+    tol: { hist: 0.1 }
   },
   {
     name: 'AsyncPF',
@@ -63,73 +63,69 @@ var inferenceProcs = [
   }
 ];
 
-var performTests = function (modelName, proc, test) {
-
-  var allExpected = loadExpected(modelName);
-
-  var inferenceFunc = proc.func || proc.name;
-  var inferenceArgs = getArgs(proc, modelName);
-
+var performTest = function(modelName, testDef, test) {
+  var expectedResults = loadExpected(modelName);
+  var inferenceFunc = testDef.func || testDef.name;
+  var inferenceArgs = getArgs(testDef, modelName);
   var progText = [
     loadModel(modelName),
     inferenceFunc, '(model,', inferenceArgs, ');'
   ].join('');
 
-  //console.log([inferenceProc.name, modelName, inferenceArgs]);
+  //console.log([testDef.name, modelName, inferenceArgs]);
 
-  webppl.run(progText, function (s, erp) {
+  webppl.run(progText, function(s, erp) {
     var hist = getHist(erp);
-    _.each(allExpected, function (expected, testName) {
+    _.each(expectedResults, function(expected, testName) {
       // The tests to run for a particular model are determined by the contents
       // of the expected results JSON file.
-      assert(tests[testName], 'Unexpected key "' + testName + '"');
-      var tolerance = getTolerance(proc, modelName, testName);
-      tests[testName](hist, expected, test, tolerance);
+      assert(testFunctions[testName], 'Unexpected key "' + testName + '"');
+      var tolerance = getTolerance(testDef, modelName, testName);
+      //console.log('\t', [testName, tolerance]);
+      testFunctions[testName](test, hist, expected, tolerance);
     });
   });
 
   test.done();
 };
 
-var getArgs = function (proc, model) {
-  var args = (proc[model] && proc[model].args) || proc.args;
+var getArgs = function(testDef, model) {
+  var args = (testDef[model] && testDef[model].args) || testDef.args;
   return JSON.stringify(args).slice(1, -1);
 };
 
-var getTolerance = function (proc, model, test) {
+var getTolerance = function(testDef, model, test) {
   // The tolerance used for a particular test is determined by taking the first
-  // defined value from the following.
-  //
-  // 1. infProc[model].tol[test]
-  // 2. infProc.tol[test]
+  // (defined) value from the following.
+
+  // 1. testDef[model].tol[test]
+  // 2. testDef.tol[test]
   // 3. defaultTol
 
   var defaultTol = 0.0001;
-  var val = _.chain([
-    proc[model] && proc[model].tol && proc[model].tol[test],
-    proc.tol && proc.tol[test],
+  return _.chain([
+    testDef[model] && testDef[model].tol && testDef[model].tol[test],
+    testDef.tol && testDef.tol[test],
     defaultTol
   ]).reject(_.isUndefined).first().value();
-  //console.log([proc.name, model, test, val]);
-  return val;
 };
 
-var testStatistic = function (statistic, name, hist, expected, test, tolerance) {
+var testStatistic = function(test, statistic, name, hist, expected, tolerance) {
   var actual = statistic(hist);
   test.ok(
-    Math.abs(actual - expected) < tolerance,
-    ['Expected ', name, ': ', expected, ', actual: ', actual].join(''));
+      Math.abs(actual - expected) < tolerance,
+      ['Expected ', name, ': ', expected, ', actual: ', actual].join(''));
 };
 
-var tests = {
-  hist: function (hist, expected, test, tolerance) {
+var testFunctions = {
+  hist: function(test, hist, expected, tolerance) {
     test.ok(util.histsApproximatelyEqual(hist, expected, tolerance));
   },
-  mean: _.partial(testStatistic, util.expectation, 'mean'),
-  std: _.partial(testStatistic, util.std, 'std')
+  mean: _.partial(testStatistic, _, util.expectation, 'mean'),
+  std: _.partial(testStatistic, _, util.std, 'std')
 };
 
-var getHist = function (erp) {
+var getHist = function(erp) {
   var hist = {};
   erp.support().forEach(function(value) {
     hist[value] = Math.exp(erp.score([], value));
@@ -137,27 +133,27 @@ var getHist = function (erp) {
   return util.normalizeHist(hist);
 };
 
-var getModelNames = function () {
+var getModelNames = function() {
   var filenames = fs.readdirSync(testDataDir + 'models2/');
-  return _.map(filenames, function (fn) { return fn.split('.')[0]; });
+  return _.map(filenames, function(fn) { return fn.split('.')[0]; });
 };
 
-var loadModel = function (modelName) {
+var loadModel = function(modelName) {
   var filename = testDataDir + 'models2/' + modelName + '.wppl';
   return fs.readFileSync(filename, 'utf-8');
 };
 
-var loadExpected = function (modelName) {
+var loadExpected = function(modelName) {
   var filename = testDataDir + 'expected2/' + modelName + '.json';
   return JSON.parse(fs.readFileSync(filename, 'utf-8'));
 };
 
-var generateTestCases = function () {
-  _.each(getModelNames(), function (modelName) {
-    _.each(inferenceProcs, function (proc) {
-      if (_.isUndefined(proc.only) || _.includes(proc.only, modelName)) {
-        var testName = modelName + proc.name;
-        exports[testName] = _.partial(performTests, modelName, proc);
+var generateTestCases = function() {
+  _.each(getModelNames(), function(modelName) {
+    _.each(testDefinitions, function(testDef) {
+      if (_.isUndefined(testDef.only) || _.includes(testDef.only, modelName)) {
+        var testName = modelName + testDef.name;
+        exports[testName] = _.partial(performTest, modelName, testDef);
       }
     });
   });
