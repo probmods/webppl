81d55b7bf2e2210da8c40c93cd28bde2e43b97b4
Siddharth Narayanaswamy <siddharth@iffsid.com>
2015-06-29 21:37:03 -0700

    simplified gradient ascent (correction with score resetting)

--- src/inference/gradientAscent.js
+++ src/inference/gradientAscent.js
@@ -23,6 +23,10 @@ module.exports = function(env) {
     this.stepSize = stepSize;
     this.hist = {};
 
+    this.sites = {}
+    this.currScore = 0;
+    this.counterfactualUpdate = false;
+
     this.wpplFn = wpplFn;
     this.originalStore = s;
     this.k = k;
@@ -46,12 +50,11 @@ module.exports = function(env) {
     return ad.untapify(this.currScore) === -Infinity
   }
 
-  Grad.prototype.run = function() {
-    this.sites = {}
-    this.currScore = 0;
-    this.counterfactualUpdate = false;
-    this.startRecompAddress = undefined;
-    this.s = this.originalStore;
+  Grad.prototype.run = function(counterfactualUpdate) {
+    if (counterfactualUpdate) {
+      this.counterfactualUpdate = true
+      this.currScore = 0;
+    }
     return this.wpplFn(this.s, env.exit, this.a);
   };
 
@@ -62,37 +65,36 @@ module.exports = function(env) {
 
   Grad.prototype.sample = function(s, k, a, erp, params) {
     var val;
-    if (this.counterfactualUpdate) {
+    if (this.counterfactualUpdate) { // update old value instead of sampling
       var lk = this.sites[a];
-      if (lk)                  // continuous erp with gradient
+      if (lk) {                  // continuous erp with gradient
         val = ad.tapify(lk.val.primal + (this.stepSize * sigmoid(lk.val.sensitivity)))
-      else                   // previously unseen sample
-        val = this.liftedSampler(erp, params);
+      }
+      else {                  // previously unseen sample -- wrong here
+        throw "Gradient Ascent: I shouldn't be here -- structure change!"
+        // val = this.liftedSampler(erp, params);
+      }
     } else
       val = this.liftedSampler(erp, params);
 
     var choiceScore = erp.score(params, val);
-    var newEntry = makeTraceEntry(_.clone(s), k, a, erp, params, erp.isContinuousERP(),
+    var newEntry = makeTraceEntry(_.clone(s), k, a, erp, params, erp.isContinuous(),
                                   this.currScore, choiceScore, val)
     this.sites[a] = newEntry;
     this.updateScore(choiceScore)
     if (this.isScoreInf())
       return this.exit(s);
-    if (erp.isContinuousERP() && !this.startRecompAddress)
-      this.startRecompAddress = a;
 
     return k(s, val);
   };
 
   Grad.prototype.propose = function() {
-    // compute the gradients along the whole tape
-    // this automatically sets the derivative in the value tape.
+    // compute gradients
+    // this updates the trace-entries made in `sites` so that the
+    // `val` fields ends up having the right gradients
     ad.yGradientR(this.currScore)
-    this.counterfactualUpdate = true; // so we know to compute val from gradient
-    // go to first continuous erp address and recompute things from there
-    var regenFrom = this.sites[this.startRecompAddress];
-    return this.sample(_.clone(regenFrom.store), regenFrom.k, regenFrom.addr,
-                       regenFrom.erp, regenFrom.params);
+    // recompute score from the start of the trace with counterfactual updates
+    return this.run(true)
   };
 
   Grad.prototype.updateHist = function(val) {
@@ -107,7 +109,7 @@ module.exports = function(env) {
     this.steps -= 1;
     // don't add values to histogram -- only want the final one
     // make a new proposal
-    return this.propose(val);
+    return this.propose();
   };
 
   Grad.prototype.finish = function(val) {
