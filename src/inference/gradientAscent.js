1d835730e8414c61c1d8baa614f0896acadaca46
Siddharth Narayanaswamy <siddharth@iffsid.com>
2015-07-04 16:40:54 -0700

    revamp gradient ascent to match hmc reformulation

--- src/inference/gradientAscent.js
+++ src/inference/gradientAscent.js
@@ -1,120 +1,124 @@
 ////////////////////////////////////////////////////////////////////
 // Gradient Ascent
+// TODO:
+// - annealing
 
 'use strict';
 
 var _ = require('underscore');
-var assert = require('assert');
 var erp = require('../erp.js');
-
 var ad = require('ad.js')({mode: 'r'})
+
+var T = require('../trace');
+var makeTrace = T.makeTrace
+var makeProposal = T.makeProposal
 
 module.exports = function(env) {
 
+  function getOpt(value, defaultValue) { return value === undefined ? defaultValue : value; };
+
   function sigmoid(x) { return (1 / (1 + Math.exp(-x))) - 0.5; }
 
-  function makeTraceEntry(s, k, a, erp, params, type, currScore, choiceScore, val) {
-    return {store: s, k: k, addr: a, erp: erp, params: params, type: type,
-      score: currScore, choiceScore: choiceScore, val: val};
-  }
+  function Grad(s, k, a, wpplFn, opts) {
+    this.stepSize     = getOpt(opts.stepSize, 0.1);
+    this.step         = getOpt(opts.steps, 3000);
+    this.steps        = getOpt(opts.steps, 3000);
+    this.verbosity    = getOpt(opts.verbosity, 0);
 
-  function Grad(s, k, a, wpplFn, stepSize, steps) {
-    this.steps = steps;
-    this.stepSize = stepSize;
-    this.hist = {};
+    this.trace        = undefined;
+    this.proposals    = {};
 
-    this.sites = {}
-    this.currScore = 0;
-    this.counterfactualUpdate = false;
-
-    this.wpplFn = wpplFn;
-    this.originalStore = s;
-    this.k = k;
-    this.a = a;
+    this.hist         = {};
+    this.wpplFn       = wpplFn;
+    this.s            = s;
+    this.k            = k;
+    this.a            = a;
 
     // Move old coroutine out of the way and install this as current handler.
     this.oldCoroutine = env.coroutine;
-    env.coroutine = this;
+    env.coroutine     = this;
   }
 
-  // Extract procedures that require AD-fication into prototypes
-  // Should ideally be general coroutine methods instead of inference specific.
-  Grad.prototype.liftedSampler = function(erp, params) {
-    var val = erp.sample(ad.untapify(params));
-    return (typeof val === 'number') ? ad.tapify(val) : val;
-  }
-  Grad.prototype.updateScore = function(score) {
-    this.currScore = ad.add(this.currScore, score);
-  }
-  Grad.prototype.isScoreInf = function() {
-    return ad.untapify(this.currScore) === -Infinity
-  }
-
-  Grad.prototype.run = function(counterfactualUpdate) {
-    if (counterfactualUpdate) {
-      this.counterfactualUpdate = true
-      this.currScore = 0;
-    }
+  Grad.prototype.run = function() {
+    this.trace = makeTrace();
+    this.trace.scoreUpdaterF = ad.add;
     return this.wpplFn(this.s, env.exit, this.a);
   };
 
   Grad.prototype.factor = function(s, k, a, score) {
-    this.updateScore(score)
-    return this.isScoreInf() ? this.exit(s) : k(s);
+    // add dummy trace entries to have a trace be a complete record
+    this.trace.append(s, k, a, null, null, score, null);
+    return ad.untapify(score) === -Infinity ? this.exit(s) : k(s);
   };
 
   Grad.prototype.sample = function(s, k, a, erp, params) {
-    var val;
-    if (this.counterfactualUpdate) { // update old value instead of sampling
-      var lk = this.sites[a];
-      if (lk) {                  // continuous erp with gradient
-        val = ad.tapify(lk.val.primal + (this.stepSize * sigmoid(lk.val.sensitivity)))
-      }
-      else {                  // previously unseen sample -- wrong here
-        throw "Gradient Ascent: I shouldn't be here -- structure change!"
-        // val = this.liftedSampler(erp, params);
-      }
-    } else
-      val = this.liftedSampler(erp, params);
-
-    var choiceScore = erp.score(params, val);
-    var newEntry = makeTraceEntry(_.clone(s), k, a, erp, params, erp.isContinuous(),
-                                  this.currScore, choiceScore, val)
-    this.sites[a] = newEntry;
-    this.updateScore(choiceScore)
-    if (this.isScoreInf())
-      return this.exit(s);
-
-    return k(s, val);
+    var proposal = this.proposals[a]; // has a proposal been made for this address?
+    var _value = proposal ? valueUpdater.call(this, proposal) : erp.sample(ad.untapify(params))
+    var value = erp.isContinuous() ? ad.tapify(_value) : _value;
+    var score = erp.score(params, value);
+    if (this.verbosity > 3)
+      console.log('Sampling:', erp.sample.name, params, ad.untapify(value), ad.untapify(score))
+    this.trace.append(s, k, a, erp, params, score, value);
+    return ad.untapify(score) === -Infinity ? this.exit(s) : k(s, value);
   };
+
+  Grad.prototype.computeGradient = function() {
+    // compute gradient on trace -- updates value sensitivities in trace entries
+    ad.yGradientR(this.trace.score());
+    if (this.verbosity > 2)
+      this.trace.forEach(function(te) {
+        if (te.isContinuous())
+          console.log('Gradient ' + te.address + ': ' + te.erpValue.sensitivity);
+      })
+  }
 
   Grad.prototype.propose = function() {
-    // compute gradients
-    // this updates the trace-entries made in `sites` so that the
-    // `val` fields ends up having the right gradients
-    ad.yGradientR(this.currScore)
-    // recompute score from the start of the trace with counterfactual updates
-    return this.run(true)
+    // (re)initialize proposals
+    this.proposals = {};
+    // compute gradient
+    this.computeGradient();
+    // record gradients in proposals
+    var cc = this;
+    this.trace.forEach(function(entry) {
+      if (entry.isContinuous())
+        cc.proposals[entry.address] = makeProposal(ad.untapify(entry.erpValue),
+                                                   entry.erpValue.sensitivity);
+    })
+    // recompute score from the start of the trace
+    return this.run()
   };
 
-  Grad.prototype.updateHist = function(val) {
-    var l = JSON.stringify(val.primal);
-    if (this.hist[l] === undefined) this.hist[l] = {prob: 0, val: val.primal};
-    this.hist[l].prob += 1;
+  function valueUpdater(proposal) {
+    return proposal.value + (this.stepSize * sigmoid(proposal.gradient));
   }
 
   Grad.prototype.exit = function(s, val) {
-    if (this.steps === 0)
-      return this.finish(val);
-    this.steps -= 1;
-    // don't add values to histogram -- only want the final one
+    // rejection initializer
+    if (this.step === this.steps && ad.untapify(this.trace.score()) === -Infinity) {
+      if (this.verbosity > 1) console.log('Rejecting first trace! Rerunning...');
+      return this.run();
+    }
+
+    if (this.step === 0)
+      return this.finish();
+    this.step -= 1;
+
+    this.updateHist(val)
+
     // make a new proposal
     return this.propose();
   };
 
-  Grad.prototype.finish = function(val) {
+  Grad.prototype.updateHist = function(val) {
+    var v = ad.untapify(val);   // fixme: this is a hack
+    var l = JSON.stringify(v);
+    if (this.hist[l] === undefined) this.hist[l] = {prob: 0, val: v};
+    this.hist[l].prob += 1;
+  }
+
+  Grad.prototype.finish = function() {
     // add last value into hist and build erp
-    this.updateHist(val);
+    // this.updateHist(val);
     var dist = erp.makeMarginalERP(this.hist);
 
     var k = this.k;
@@ -124,8 +128,8 @@ module.exports = function(env) {
     return k(this.oldStore, dist);
   }
 
-  function grad(s, cc, a, wpplFn, stepsize, steps) {
-    return new Grad(s, cc, a, wpplFn, stepsize, steps).run();
+  function grad(s, cc, a, wpplFn, opts) {
+    return new Grad(s, cc, a, wpplFn, opts).run();
   }
 
   return {
