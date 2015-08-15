6449be5713997f496139d835ced6d41dabf8530f
Siddharth Narayanaswamy <siddharth@iffsid.com>
2015-08-15 04:07:41 -0700

    fix print under unified toJSON return

--- src/erp.js
+++ src/erp.js
@@ -107,16 +107,12 @@ ERP.prototype.toJSON = function() {
 
 ERP.prototype.print = function() {
   var serialized = this.toJSON();
-  if (typeof serialized === 'string') {
-    console.log(serialized);
-  } else {
-    console.log(this.name + ':');
-    _.map(serialized.erp, function(v, k) { return [k, v.prob]; })
-      .sort(function(a, b) { return b[1] - a[1]; })
-      .forEach(function(val) {
-          console.log('    ' + val[0] + ' : ' + Math.exp(val[1]));
-        })
-  }
+  console.log(serialized.name + ':');
+  _.map(serialized.erp, function(v, k) { return [k, v.prob]; })
+    .sort(function(a, b) { return b[1] - a[1]; })
+    .forEach(function(val) {
+        console.log('    ' + val[0] + ' : ' + Math.exp(val[1]));
+      })
 };
 
 // ERP deserializer
