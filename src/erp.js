probmods/webppl

commit 24a9a07db9ec8a2fe8389734c2512d145fe898b4

Check required functions are passed to constructor

src/erp.js
@@ -29,6 +29,7 @@ var assert = require('assert');
 var LOG_2PI = 1.8378770664093453;
 
 function ERP(obj) {
+  assert(obj.sample && obj.score, 'ERP must implement sample and score.');
   _.extendOwn(this, obj);
 }
 

commit 7c4b3ff3064653005dc53acbb99db94db4a4e46f

Fix bug introduced in 24a9a07.
withParameters was failing as it didn't pass an object with sample and
score functions to the ERP constructor.

src/erp.js
@@ -67,8 +67,7 @@ ERP.prototype.entropy = function() {
 ERP.prototype.parameterized = true;
 
 ERP.prototype.withParameters = function(params) {
-  var erp = new ERP();
-  _.forEach(this, function(v, k) {erp[k] = v;});
+  var erp = new ERP(this);
   var sampler = this.sample;
   erp.sample = function(ps) {return sampler(params)};
   var scorer = this.score;

tests/test-data/deterministic/models/withParameters.wppl
@@ -1,0 +1,1 @@
+randomIntegerERP.withParameters([2]).toJSON()