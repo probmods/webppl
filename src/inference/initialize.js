595ee14467431d3d3eb6bb24d79301b2062339b2
null-a <null-a@users.noreply.github.com>
2015-09-22 16:49:47 +0100

    Warn if MCMC init. is taking many attempts.

--- src/inference/initialize.js
+++ src/inference/initialize.js
@@ -8,11 +8,14 @@ module.exports = function(env) {
 
   // This takes a wpplFn and returns a trace which has a non-zero probability.
 
+  var warnAfter = [1e3, 1e4, 1e5, 1e6];
+
   function Initialize(s, k, a, wpplFn) {
     this.wpplFn = wpplFn;
     this.s = s;
     this.k = k;
     this.a = a;
+    this.failures = 0;
     this.coroutine = env.coroutine;
     env.coroutine = this;
   }
@@ -31,10 +34,20 @@ module.exports = function(env) {
 
   Initialize.prototype.factor = function(s, k, a, score) {
     if (score === -Infinity) {
-      return this.run();
+      return this.fail();
     }
     this.trace.score += score;
     return k(s);
+  };
+
+  Initialize.prototype.fail = function() {
+    this.failures += 1;
+    var ix = warnAfter.indexOf(this.failures);
+    if (ix >= 0) {
+      console.log(['Initialization warning [', (ix + 1), '/', warnAfter.length,
+                   ']: Trace not initialized after ', this.failures, ' attempts.'].join(''));
+    }
+    return this.run();
   };
 
   Initialize.prototype.exit = function(s, val) {
