751f2069dda063f0feecd5c1e920bfe15056bc78
null-a <null-a@users.noreply.github.com>
2015-11-10 12:21:26 +0000

    Add tests for caching transform helper functions.

---
+++ tests/test-caching.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _ = require('underscore');
+var parse = require('esprima').parse;
+var caching = require('../src/transforms/caching');
+
+var hasCachingDirectiveTests = {
+  test1: { code: "'no caching'", expected: true },
+  test2: { code: '"no caching";\n', expected: true },
+  test3: { code: '\n"no caching";\n1+1', expected: true },
+  test4: { code: '1 + 1', expected: false },
+  test5: { code: "1 + 1;\n'no caching'", expected: false },
+  test6: { code: '', expected: false }
+};
+
+var transformRequiredTests = {
+  test1: { code: 'IncrementalMH(model, 0);', expected: true },
+  test2: { code: 'Enumerate(model);', expected: false }
+};
+
+function generateTests(cases, testFn) {
+  return _.mapObject(cases, function(caseDef) {
+    return function(test) {
+      test.strictEqual(testFn(parse(caseDef.code)), caseDef.expected);
+      test.done();
+    };
+  });
+}
+
+module.exports = {
+  hasNoCachingDirective: generateTests(hasCachingDirectiveTests, caching.hasNoCachingDirective),
+  transformRequired: generateTests(transformRequiredTests, caching.transformRequired)
+};
