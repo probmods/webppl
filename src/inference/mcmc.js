6ede011a7ec6d0e316e7619cd32c0729a922f54e
null-a <null-a@users.noreply.github.com>
2015-11-24 08:38:02 +0000

    Remove unused require statements.

--- src/inference/mcmc.js
+++ src/inference/mcmc.js
@@ -1,9 +1,7 @@
 'use strict';
 
 var _ = require('underscore');
-var assert = require('assert');
 var util = require('../util');
-var Query = require('../query').Query;
 var aggregation = require('../aggregation');
 
 module.exports = function(env) {
