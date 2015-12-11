5988c5e0a39ee4666953f6ed1407e01f4e30a408
Long Ouyang <longouyang@gmail.com>
2015-12-11 09:43:53 -0800

    Stricter equality check

--- src/inference/enumerate.js
+++ src/inference/enumerate.js
@@ -91,7 +91,7 @@ module.exports = function(env) {
   Enumerate.prototype.factor = function(s, cc, a, score) {
     // Update score and continue
     this.score += score;
-    if (this.score == -Infinity) {
+    if (this.score === -Infinity) {
       return this.exit();
     }
     return cc(s);
