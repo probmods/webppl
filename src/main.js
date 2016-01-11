3206ef2d6c7da23efc8c8e92d8787b10d576b527
null-a <null-a@users.noreply.github.com>
2016-01-11 13:58:50 +0000

    Move comment to issue tracker.

--- src/main.js
+++ src/main.js
@@ -71,8 +71,6 @@ function headerPackage() {
   // Create a pseudo package from the header.
   var code = fs.readFileSync(__dirname + '/header.wppl', 'utf8');
   var headerMacroModule = fs.readFileSync(__dirname + '/headerMacros.sjs', 'utf8');
-  // HACK: This could be problematic if the macros move within the
-  // ad.js package across versions.
   var adMacroModule = fs.readFileSync(__dirname + '/../node_modules/ad.js/macros/index.js', 'utf8');
   return { wppl: [code], macros: [headerMacroModule, adMacroModule] };
 }
