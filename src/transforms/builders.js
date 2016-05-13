04474d1b7a1fdd7ffddfc50d04dafe28e3c7030b
null-a <null-a@users.noreply.github.com>
2016-05-13 15:54:27 +0100

    Wrap builders with versions that set the node location.

---
+++ src/transforms/builders.js
@@ -0,0 +1,19 @@
+var builders = require('ast-types').builders;
+var _ = require('underscore');
+
+function isLocationNode(node) {
+  return _.has(node, 'start') && _.has(node, 'end');
+}
+
+module.exports = _.mapObject(builders, function(builder, name) {
+  return function() {
+    var args = _.toArray(arguments);
+    if (args.length > 0 && isLocationNode(_.last(args))) {
+      var node = builder.apply(null, args.slice(0, -1));
+      node.loc = _.last(args);
+      return node;
+    } else {
+      return builder.apply(null, args);
+    }
+  };
+});
