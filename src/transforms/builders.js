9d58240e291712967f3c687bd9339af372ab3628
null-a <null-a@users.noreply.github.com>
2016-05-13 19:50:06 +0100

    Add commentary to wrapped builders.

--- src/transforms/builders.js
+++ src/transforms/builders.js
@@ -1,6 +1,13 @@
 var builders = require('ast-types').builders;
 var _ = require('underscore');
 
+// The ast-type builders don't provide a convenient way to set the
+// source location of a built node. As a work-around, we wrap each
+// builder function with a new function that takes the source location
+// as an extra argument and adds it to the new node. The source
+// location argument is optional, and is given as the final argument
+// when present.
+
 function isLocationNode(node) {
   return _.has(node, 'start') && _.has(node, 'end');
 }
