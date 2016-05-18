16d4b8ff906c7b9fb31bdf74599f8365c3fcff99
null-a <null-a@users.noreply.github.com>
2016-05-18 11:29:25 +0100

    Add compound nets for recurrent units.

--- daipp/daipp.js
+++ daipp/daipp.js
@@ -7,8 +7,7 @@ var dists = require('../src/dists.js');
 var LRU = require('lru-cache');
 var serialize = require('../src/util').serialize;
 
-// Controls whether adnn debug checks are enabled for init, update and
-// predict nets.
+// Controls whether adnn debug checks are enabled for nets.
 var debug = true;
 
 //this sets the size of the context network throughout daipp
@@ -35,6 +34,65 @@ function xavierInit(t) {
     throw 'xavierInit: Unexpected rank.';
   }
 }
+
+var makeRU = function(hdim, xdim, name) {
+  // Create a simple recurrent unit.
+  var net =  nn.sequence(
+    [nn.linear(hdim+xdim, hdim), nn.tanh],
+    name + ':activation',
+    debug);
+  var fn = function(args) {
+    var hprev = args[0];
+    var x = args[1];
+    var hprevx = nn.concat.eval(hprev, x);
+    return net.eval(hprevx);
+  };
+  return nn.compound(fn, [net, nn.concat], name);
+};
+
+var makeGRU = function(hdim, xdim, name) {
+  // Create a Gated Recurrent Unit.
+
+  // Update gate.
+  // z = sigmoid(Wz . [hprev, x])
+  var updateNet = nn.sequence(
+    [nn.linear(hdim+xdim, hdim), nn.sigmoid],
+    name + ':update',
+    debug);
+
+  // Reset gate.
+  // r = sigmoid(Wr . [hprev, x])
+  var resetNet = nn.sequence(
+    [nn.linear(hdim+xdim, hdim), nn.sigmoid],
+    name + ':reset',
+    debug);
+
+  // Candidate activation.
+  // h' = tanh(W . [r*hprev, x])
+  var candidateNet = nn.sequence(
+    [nn.linear(hdim+xdim, hdim), nn.tanh],
+    name + ':activation',
+    debug);
+
+  // Take a single arg to play nicely with daipp's nneval.
+  var fn = function(args) {
+    var hprev = args[0];
+    var x = args[1];
+    var hprevx = nn.concat.eval(hprev, x);
+    var z = updateNet.eval(hprevx);
+    var r = resetNet.eval(hprevx);
+    var candidate = candidateNet.eval(nn.concat.eval(nn.mul.eval(r, hprev), x));
+    // Output.
+    // h = (1-z)*hprev + z*h'
+    var h = nn.add.eval(nn.mul.eval(nn.add.eval(nn.mul.eval(z, -1), 1), hprev), nn.mul.eval(z, candidate));
+    return h;
+  };
+
+  var subnets = [updateNet, resetNet, candidateNet, nn.concat, nn.mul, nn.add];
+  return nn.compound(fn, subnets, name);
+};
+
+// TODO: Add "bilinear resnet", LSTM.
 
 // dritchie: We need a function that wraps any call to nn.eval(), which will do parameter registration
 // IMPORTANT: We assume that every nn has been given a name, which we use for the param name/address
@@ -315,5 +373,7 @@ module.exports = {
   val2vec: val2vec,
   vec2dist: vec2dist,
   orderedValues: orderedValues,
-  debug: debug
+  debug: debug,
+  makeRU: makeRU,
+  makeGRU: makeGRU
 }
