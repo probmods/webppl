a1d5a9524f2b77bb602c2d6e3c4f9761ae8c235c
null-a <null-a@users.noreply.github.com>
2016-06-02 12:39:22 +0100

    Remove stale code.

--- src/tensor.js
+++ src/tensor.js
@@ -6,30 +6,6 @@ var Tensor = require('adnn/tensor');
 var statistics = require('./statistics');
 var special = require('./special');
 
-// TODO: toString should return a string (and not an array) so that it
-// plays nicely with +.
-
-// Tensor.prototype.toString = function() {
-//   return 'Tensor([' + this.dims + '])';
-// };
-
-// Tensor.prototype.inspect = function() {
-//   if (this.length <= 25) {
-//     // TODO: Check the browserify shim for util works as expected.
-//     return inspect(this.toArray());
-//   } else {
-//     var arr = this.toFlatArray();
-//     return 'Tensor(' + inspect({
-//       dims: this.dims,
-//       mean: statistics.mean(arr),
-//       std: statistics.sd(arr),
-//       min: this.minreduce(),
-//       max: this.maxreduce(),
-//       allFinite: _.all(arr, _.isFinite)
-//     }) + ')';
-//   }
-// };
-
 Tensor.prototype.logGamma = function() {
   var out = new Tensor(this.dims);
   var n = this.data.length;
