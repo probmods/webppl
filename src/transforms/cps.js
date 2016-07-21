667aff7cd804e9fc44396a0622d494d308afc0ca
Long Ouyang <longouyang@gmail.com>
2016-07-21 09:39:09 -0700

    Switch from assert to throw

--- src/transforms/cps.js
+++ src/transforms/cps.js
@@ -55,26 +55,25 @@ function atomize(node, metaK) {
       });
     }),
     clause(Syntax.AssignmentExpression, function(left, right) {
-      var errorMessage = function() {
-        var line = left.loc.start.line;
-        var msg;
-        if (left.type === Syntax.MemberExpression) {
-          if (left.object.name !== 'globalStore') {
-            msg = 'You tried to assign to a field of ' + left.object.name +
-                ', but you can only assign to fields of globalStore';
-          }
-        } else {
-          if (left.name == 'globalStore') {
-            msg = 'You must assign to a field of globalStore, not globalStore itself'
-          } else {
-            msg = 'Did you mean var ' + left.name + ' = ?'
-          }
+      var err = false, line, msg;
+      if (left.type === Syntax.MemberExpression) {
+        if (!(left.object.name === 'globalStore')) {
+          err = true;
+          msg = 'You tried to assign to a field of ' + left.object.name +
+              ', but you can only assign to fields of globalStore';
         }
-        return 'Line ' + line + ': ' + msg;
+      } else {
+        err = true;
+        if (left.name === 'globalStore') {
+          msg = 'You must assign to a field of globalStore, not globalStore itself';
+        } else {
+          msg = 'Did you mean var ' + left.name + ' = ?'
+        }
       }
-      assert(left.type === Syntax.MemberExpression &&
-             left.object.name === 'globalStore',
-             errorMessage());
+      if (err) {
+        line = left.loc.start.line;
+        throw new Error('Line ' + line + ': ' + msg);
+      }
       return atomize(left, function(left) {
         return atomize(right, function(right) {
           return metaK(build.assignmentExpression(node.operator, left, right));
