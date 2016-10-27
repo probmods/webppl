bfd5f8446d84f802595ade98337eae937101e1f8
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-10-26 19:48:27 -0700

    Add in-memory param store

---
+++ src/params/store.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// This is a simple in-memory implementation of a parameter store.
+
+var _ = require('underscore');
+var paramStruct = require('./struct');
+
+var store = {};
+
+function getParams(id) {
+  if (_.has(store, id)) {
+    return paramStruct.deepCopy(store[id]);
+  } else {
+    return {};
+  }
+}
+
+function incParams(id, params, deltas) {
+  if (!_.has(store, id)) {
+    store[id] = {};
+  }
+  var table = store[id];
+  _.defaults(table, params);
+  paramStruct.addEq(table, deltas);
+  return paramStruct.deepCopy(table);
+}
+
+module.exports = {
+  getParams: getParams,
+  incParams: incParams
+};
