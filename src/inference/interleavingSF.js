3477b6a25e8be9f87a8d93173562ca48d599b177
Eric-H-Ren <rhilogin@gmail.com>
2017-02-26 00:17:44 -0800

    simplify interleavingSF.js

--- src/inference/interleavingSF.js
+++ src/inference/interleavingSF.js
@@ -5,29 +5,18 @@
 
 var _ = require('lodash');
 var util = require('../util');
-var CountAggregator = require('../aggregation/CountAggregator');
 var ad = require('../ad');
-var guide = require('../guide');
 
 module.exports = function(env) {
 
   function InterleavingSF(s, k, a, wpplFn, options) {
-    this.opts = util.mergeDefaults(options, {
-      samples: 1,
-      guide: false, // true = sample guide, false = sample target
-      verbose: false
-    });
-
+    this.opts = {samples: 1};
     this.wpplFn = wpplFn;
     this.s = s;
     this.k = k;
     this.a = a;
-    this.guideRequired = this.opts.guide;
     this.hasFactor = false;
     this.interleavingSampleFactor = false;
-
-    this.factorWarningIssued = false;
-
     this.coroutine = env.coroutine;
     env.coroutine = this;
   }
@@ -36,9 +25,6 @@ module.exports = function(env) {
 
     run: function() {
 
-      var hist = new CountAggregator();
-      var logWeights = [];   // Save total factor weights
-
       return util.cpsLoop(
           this.opts.samples,
 
@@ -46,8 +32,6 @@ module.exports = function(env) {
           function(i, next) {
             this.logWeight = 0;
             return this.wpplFn(_.clone(this.s), function(s, val) {
-              logWeights.push(this.logWeight);
-              hist.add(val);
               return next();
             }.bind(this), this.a);
           }.bind(this),
@@ -55,11 +39,6 @@ module.exports = function(env) {
           // Continuation.
           function() {
             env.coroutine = this.coroutine;
-            var dist = hist.toDist();
-            if (!this.opts.guide) {
-              var numSamples = this.opts.samples;
-              dist.normalizationConstant = util.logsumexp(logWeights) - Math.log(numSamples);
-            }
             return this.k(this.s, this.interleavingSampleFactor);
           }.bind(this));
 
@@ -69,30 +48,13 @@ module.exports = function(env) {
       if (this.hasFactor) {
         this.interleavingSampleFactor = true;
       }
-      if (this.opts.guide) {
-        options = options || {};
-        return guide.getDist(
-            options.guide, options.noAutoGuide, dist, env, s, a,
-            function(s, maybeGuideDist) {
-              var d = maybeGuideDist || dist;
-              return k(s, d.sample());
-            });
-      } else {
-        return k(s, dist.sample());
-      }
+      return k(s, dist.sample());
     },
 
     factor: function(s, k, a, score) {
       if (!this.hasFactor) {
         this.hasFactor = true;
       }
-      if (!this.opts.guide && !this.factorWarningIssued) {
-        this.factorWarningIssued = true;
-        var msg = 'Note that factor, condition and observe statements are ' +
-            'ignored when forward sampling from a model.';
-        util.warn(msg);
-      }
-      this.logWeight += ad.value(score);
       return k(s);
     },
 
